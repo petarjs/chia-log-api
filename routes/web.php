a83f8ce2c365a43ff5bf4b401e4893bb20e6c881
Petar Slovic <petar.slovic@gmail.com>
2021-07-08 21:03:52 +0200

    add view

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LogController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->middleware('fw-only-whitelisted');
+Route::get('/logs', [LogController::class, 'index']);
