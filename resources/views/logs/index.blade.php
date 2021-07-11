96e504ef415b31b385aa2857fc0603b00d10ec4f
Petar Slovic <petar.slovic@gmail.com>
2021-07-11 08:11:38 +0200

    process logs

--- resources/views/logs/index.blade.php
+++ resources/views/logs/index.blade.php
@@ -8,12 +8,12 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+                <div class="js-log-container p-6 bg-black border-b border-gray-200 font-mono text-white" style="max-height: 580px; overflow: scroll">
                     <table>
                         @foreach($logLines as $log)
                             <tr>
-                                <td>{{$log->created_at}}</td>
-                                <td>{{$log->line}}</td>
+                                <td style="vertical-align: top; min-width: 200px">{{$log->created_at}}</td>
+                                <td class="text-green-600">{{$log->line}}</td>
                             </tr>
                         @endforeach
                     </table>
@@ -22,3 +22,10 @@
         </div>
     </div>
 </x-app-layout>
+
+<script>
+    document.addEventListener('DOMContentLoaded', () => {
+        const $logContainer = document.querySelector('.js-log-container');
+        $logContainer.scrollTop = $logContainer.scrollHeight;
+    })
+</script>
