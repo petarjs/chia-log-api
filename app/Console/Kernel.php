b5089652e465cab3f663841b8f64a80a8a0a6c2d
Petar Slovic <petar.slovic@gmail.com>
2021-07-14 23:53:08 +0200

    tet

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('system-summary')->hourly();
+        \Log::debug('Executing system summary');
+        $schedule->command('system-summary')->everyMinute();
     }
 
     /**
